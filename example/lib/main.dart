160ea2c612b523f11f5e7882a22379d24eea5b3c
Djcharles26 <jlaraanaya@gmail.com>
2021-04-16 17:55:22 -0500

    Updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:example/pages/AddCreditCard.dart';
 import 'package:example/pages/Home.dart';
 import 'package:flutter/material.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      routes: <String, WidgetBuilder>{
-        "/addCard": (ctx)=> AddCard()
-      },
-      home: Home()
-    );
+        title: 'Material App',
+        routes: <String, WidgetBuilder>{"/addCard": (ctx) => AddCard()},
+        home: Home());
   }
 }
-
